c842d08fc5ba28440b59bc971b00778d1c445408
Adam Simon <adampersimonal@gmail.com>
2023-04-25 15:46:07 -0500

    Created rolling animation

--- scripts/die.js
+++ scripts/die.js
@@ -6,10 +6,21 @@ class Die {
     this.element.className = "die";
     this.element.id = config.id;
     this.element.innerHTML = this.value;
+
+    this.element.style.animationDelay = `${this.element.id * 100}ms`;
+    this.element.onanimationend = () => {
+      this.element.classList.remove("rolling");
+      this.render();
+    };
   }
 
   roll() {
     this.value = getRandomDieValue();
+    this.element.innerHTML = "";
+    this.element.classList.add("rolling");
+  }
+
+  render() {
     this.element.innerHTML = this.value;
   }
 }
